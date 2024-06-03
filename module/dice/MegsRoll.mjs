edcaf04bad97b80c48dbbcb55cbc334bc466b557
codemonkey1972 <codemonkey1972@gmail.com>
2024-06-02 22:55:38 -0400

    Roll object

--- module/dice/MegsRoll.mjs
+++ module/dice/MegsRoll.mjs
@@ -2,9 +2,6 @@ export class MegsRoll extends Roll {
     
     async toMessage(messageData={}, {rollMode, create=true}={}) {
 
-        // Perform the roll, if it has not yet been rolled
-        if ( !super._evaluated ) await super.evaluate({async: true});
-
         await ChatMessage.create(
             {
               content: dialogHtml
