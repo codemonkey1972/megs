e47c03810a184c8719d3ef936908944202f94f2c
Jeff Barnes <codemonkey1972@gmail.com>
2024-12-01 09:31:39 -0600

    Bug fix

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -38,6 +38,7 @@ export class MEGSActor extends Actor {
         img: skillData.img ? 
         system: skillData,
       };
+      console.error (itemData);
       delete itemData.system['type'];
 
       const skill = await MEGSItem.implementation.create(itemData, { parent: this.actor });
