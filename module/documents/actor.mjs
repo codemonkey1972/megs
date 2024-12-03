49dcf0c41511812954da4afbd972cea0c9572129
Jeff Barnes <codemonkey1972@gmail.com>
2024-12-03 01:04:48 -0500

    Skills

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -10,6 +10,8 @@ export class MEGSActor extends Actor {
   async _preCreate(data, options, user) {
     await super._preCreate(data, options, user);
 
+    const data = require('systems/megs/assets/data/skills.json');
+    console.error(data);
       // _loadData('systems/megs/assets/data/skills.json').then(async (skillsJson) => {
 
           // let allGameSkills = [];
