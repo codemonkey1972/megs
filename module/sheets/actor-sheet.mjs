60a1b74f6933a76585de9b2e180e0e5fb28d7ea3
Jeff Barnes <codemonkey1972@gmail.com>
2025-01-19 15:01:59 -0500

    Cleanup

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -219,7 +219,6 @@ export class MEGSActorSheet extends ActorSheet {
     // TODO replace this with active effects
 
     // calculate initiativeBonus
-    console.error("TEST8");
     let initiativeBonus = context.document.system.attributes.dex.value + context.document.system.attributes.int.value
         + context.document.system.attributes.infl.value;
 
@@ -342,7 +341,6 @@ export class MEGSActorSheet extends ActorSheet {
         i.subskills = [];
         if (i.system.aps === 0) {
           i.unskilled = true;
-          console.error("TESI9"); // TODO delete
           i.linkedAPs = this.object.system.attributes[i.system.link].value;
         } else {
           i.unskilled = false;
