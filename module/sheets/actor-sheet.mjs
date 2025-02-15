ea313624fa989f854f174a3070920616a1d12fb0
Jeff Barnes <codemonkey1972@gmail.com>
2025-02-15 09:28:46 -0600

    selectOptions for power sheet

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -135,7 +135,6 @@ export class MEGSActorSheet extends ActorSheet {
 
             owner.items.forEach((element) => {
               if (element.type === MEGS.itemTypes.gadget) {
-                console.error(element); // TODO delete
 
                 // store linked vehicle item
                 if (element._id === context.system.linkedItemId) {
