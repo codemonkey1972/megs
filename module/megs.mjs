ff2ebe091232719f9d0c9ba276a1930f6a68aa6f
Jeff Barnes <codemonkey1972@gmail.com>
2025-03-14 13:18:32 -0400

    Testing

--- module/megs.mjs
+++ module/megs.mjs
@@ -330,7 +330,13 @@ Handlebars.registerHelper('shouldShowRow', function(index, hasAttributes, option
 Handlebars.registerPartial('plusMinusInput', function(args) {
   const classes = args.classes ? args.classes : '';
   const max = (args.max && !isNaN(args.max)) ? args.max : '';
-  const min = args.min === '0' ? 0 : (args.min && !isNaN(args.min)) ? args.min : '';
+//  const min = args.min === '0' ? 0 : (args.min && !isNaN(args.min)) ? args.min : '';
+  let min = 0;
+  if (args.min && !isNaN(args.min)) {
+    min = args.min;
+  }
+  console.error(min, args.min); // TODO delete
+
   const valueTag = args.hasValue ? ".value" : "";
   const value = (args.value && !isNaN(args.value)) ? args.value : '0';
 
