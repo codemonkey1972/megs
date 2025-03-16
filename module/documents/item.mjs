893781b878e7abce04749e36ed0174f01933c711
Jeff Barnes <codemonkey1972@gmail.com>
2025-03-16 11:12:24 -0400

    debugging attributesForLink

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -77,7 +77,7 @@ export class MEGSItem extends Item {
       });
     } else {
       console.error("TEST1", MEGS); // TODO delete
-      Object.entries(MEGS.attributes[k]).forEach(function (label) {
+      Object.entries(MEGS.attributes).forEach(function (label) {
         console.error("TEST2", game.i18n.localize(label));
       });
 
