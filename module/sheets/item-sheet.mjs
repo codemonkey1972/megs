1a04a0308397739d2c3f57b3cc80b3a396047e37
Jeff Barnes <codemonkey1972@gmail.com>
2025-03-29 13:25:34 -0500

    Reset defaults

--- module/sheets/item-sheet.mjs
+++ module/sheets/item-sheet.mjs
@@ -127,7 +127,6 @@ export class MEGSItemSheet extends ItemSheet {
 
     if (itemData.type === MEGS.itemTypes.gadget) {
       context.items = itemData.system.items;
-      console.error(context.system.settings); // TODO delete
       this._prepareGadgetData(context);
     }
 
